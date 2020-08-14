c9cdbdd9b0b8cd5106fb2e8d8c349fd5aaff053e
Sergio Yepes <sa.yepes@uniandes.edu.co>
2020-08-13 22:18:12 -0500

    endpoints edited

--- back-end/db_connection.js
+++ back-end/db_connection.js
@@ -19,10 +19,10 @@ module.exports.default = () => {
         const event_name = event.event_name ? event.event_name : ''
         const event_place = event.event_place ? event.event_place : ''
         const event_address = event.event_address ? event.event_address : ''
-        const event_initial_date = transformDate(event.event_initial_date )
-        const event_final_date = transformDate(event.event_final_date )
+        const event_initial_date = transformDate(event.event_initial_date)
+        const event_final_date = transformDate(event.event_final_date)
         const event_type = event.event_type ? event.event_type : ''
-        const username_owner = event.username_owner ? event.username_owner : ''
+        const username_owner = event.username_owner ? event.username_owner : null
         const values = { event_name, event_place, event_address, event_initial_date, event_final_date, event_type, username_owner }
         return values
     }
@@ -49,8 +49,9 @@ module.exports.default = () => {
             try {
                 const client = createClientConn()
                 const values = buildEventValues(event)
-                const query = `INSERT INTO events(event_name, event_place, event_address, event_initial_date, event_final_date, event_type, username_owner) VALUES
-                 ('${values.event_name}', '${values.event_place}', '${values.event_address}', '${values.event_initial_date}', '${values.event_final_date}', '${values.event_type}', '${values.username_owner}') RETURNING *`
+                const usernameOwnerCheck = values.username_owner ? true : false
+                const query = `INSERT INTO events(event_name, event_place, event_address, event_initial_date, event_final_date, event_type ${usernameOwnerCheck ? ', username_owner' : ''}) VALUES
+                 ('${values.event_name}', '${values.event_place}', '${values.event_address}', '${values.event_initial_date}', '${values.event_final_date}', '${values.event_type}' ${usernameOwnerCheck ? `, '${values.username_owner}'` : ''}) RETURNING *`
 
                 result = await client.query(query).catch(e => console.error(e))
                 endClientConn(client)
