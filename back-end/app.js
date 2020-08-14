112581ec491c14d06a1800c18f612ceb700a127d
Sergio Yepes <sa.yepes@uniandes.edu.co>
2020-08-13 21:53:48 -0500

    endpoints edited

--- back-end/app.js
+++ back-end/app.js
@@ -3,32 +3,36 @@ const app = express()
 const port = 3000
 const DbConnection = require('./db_connection').default
 
+app.use(express.json())
+
 app.post('/api/create-user', async (req, res) => {
-  res.send(await DbConnection().getEvents());
+  res.json(await DbConnection().getEvents());
 })
 
 app.post('/api/api-auth', async (req, res) => {
-  res.send('done');
+  res.json({
+    msg: 'done'
+  });
 })
 
 app.get('/api/events', async (req, res) => {
-  res.send(await DbConnection().getEvents());
+  res.json(await DbConnection().getEvents());
 })
 
 app.post('/api/events', async (req, res) => {
-  res.send(await DbConnection().postEvents(req.body));
+  res.json(await DbConnection().postEvents(req.body));
 })
 
 app.get('/api/events/:event_id', async (req, res) => {
-  res.send(await DbConnection().getEventById(req.params.event_id));
+  res.json(await DbConnection().getEventById(req.params.event_id));
 })
 
 app.put('/api/events/:event_id', async (req, res) => {
-  res.send(await DbConnection().putEvents(req.params.event_id, req.body));
+  res.json(await DbConnection().putEvents(req.params.event_id, req.body));
 })
 
 app.delete('/api/events/:event_id', async (req, res) => {
-  res.send(await DbConnection().deleteEvent(req.params.event_id));
+  res.json(await DbConnection().deleteEvent(req.params.event_id));
 })
 
 
--- back-end/db_connection.js
+++ back-end/db_connection.js
@@ -26,38 +26,63 @@ module.exports.default = () => {
     }
     return {
         getEvents: async () => {
-            const client = createClientConn()
-            const result = await client.query('SELECT * FROM events').catch(e => console.error(e))
-            endClientConn(client)
+            let result = null
+            try {
+                const client = createClientConn()
+                result = await client.query('SELECT * FROM events').catch(e => console.error(e))
+                endClientConn(client)
+            } catch (e) {
+                console.error(e)
+            }
             return result ? result.rows : []
         },
         postEvents: async (event) => {
-            const client = createClientConn()
-            const query = 'INSERT INTO events(event_name, event_place, event_address, event_initial_date, event_final_date, event_type, username_owner) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *'
-            const values = buildEventValues(event)
-            const result = await client.query(query, values).catch(e => console.error(e))
-            endClientConn(client)
-            return result ? result.rows : []
+            let result = null
+            try {
+                const client = createClientConn()
+                const query = 'INSERT INTO events(event_name, event_place, event_address, event_initial_date, event_final_date, event_type, username_owner) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *'
+                const values = buildEventValues(event)
+                result = await client.query(query, values).catch(e => console.error(e))
+                endClientConn(client)
+            } catch (e) {
+                console.error(e)
+            }
+            return result
         },
         getEventById: async (id) => {
-            const client = createClientConn()
-            const result = await client.query('SELECT * FROM events WHERE id = ' + id).catch(e => console.error(e))
-            endClientConn(client)
+            let result = null
+            try {
+                const client = createClientConn()
+                result = await client.query('SELECT * FROM events WHERE id = ' + id).catch(e => console.error(e))
+                endClientConn(client)
+            } catch (e) {
+                console.error(e)
+            }
             return result && result.rows && result.rows.length > 0 ? result.rows[0] : null
         },
         putEvents: async (id, event) => {
-            const client = createClientConn()
-            const query = 'UPDATE events SET event_name = $1, event_place = $2, event_address = $3, event_initial_date = $4, event_final_date = $5, event_type = $6, username_owner = $7 WHERE id = $8'
-            let values = buildEventValues(event)
-            values.push(id)
-            const result = await client.query(query, values).catch(e => console.error(e))
-            endClientConn(client)
-            return result ? result.rows : []
+            let result = null
+            try {
+                const client = createClientConn()
+                const query = 'UPDATE events SET event_name = $1, event_place = $2, event_address = $3, event_initial_date = $4, event_final_date = $5, event_type = $6, username_owner = $7 WHERE id = $8'
+                let values = buildEventValues(event)
+                values.push(id)
+                result = await client.query(query, values).catch(e => console.error(e))
+                endClientConn(client)
+            } catch (e) {
+                console.error(e)
+            }
+            return result
         },
         deleteEvent: async (id) => {
-            const client = createClientConn()
-            const result = await client.query('DELETE FROM events WHERE id = ' + id).catch(e => console.error(e))
-            endClientConn(client)
+            let result = null
+            try {
+                const client = createClientConn()
+                result = await client.query('DELETE FROM events WHERE id = ' + id).catch(e => console.error(e))
+                endClientConn(client)
+            } catch (e) {
+                console.error(e)
+            }
             return result
         }
     }
