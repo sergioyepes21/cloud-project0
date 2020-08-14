1e3f20437b25adc991c6578f5a73cd7f7fbbc1e2
Sergio Yepes <sa.yepes@uniandes.edu.co>
2020-08-13 23:05:57 -0500

    endpoints edited

--- back-end/app.js
+++ back-end/app.js
@@ -6,9 +6,9 @@ const DbConnection = require('./db_connection').default
 app.use(express.json())
 
 app.post('/api/create-user', async (req, res) => {
-  res.json(await DbConnection().postUser(req.body).catch(e => res.status(500).send({
-    error : e
-  })));
+  res.json(await DbConnection().postUser(req.body).catch(e => {
+    return { error: e }
+  }))
 })
 
 app.post('/api/api-auth', async (req, res) => {
@@ -19,31 +19,31 @@ app.post('/api/api-auth', async (req, res) => {
 
 app.get('/api/events', async (req, res) => {
   res.json(await DbConnection().getEvents().catch(e => res.status(500).send({
-    error : e
+    error: e
   })));
 })
 
 app.post('/api/events', async (req, res) => {
   res.json(await DbConnection().postEvents(req.body).catch(e => res.status(500).send({
-    error : e
+    error: e
   })));
 })
 
 app.get('/api/events/:event_id', async (req, res) => {
   res.json(await DbConnection().getEventById(req.params.event_id).catch(e => res.status(500).send({
-    error : e
+    error: e
   })));
 })
 
 app.put('/api/events/:event_id', async (req, res) => {
   res.json(await DbConnection().putEvents(req.params.event_id, req.body).catch(e => res.status(500).send({
-    error : e
+    error: e
   })));
 })
 
 app.delete('/api/events/:event_id', async (req, res) => {
   res.json(await DbConnection().deleteEvent(req.params.event_id).catch(e => res.status(500).send({
-    error : e
+    error: e
   })));
 })
 
